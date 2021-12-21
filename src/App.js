93970f8193ce98eb5ec3faf3413ae8c502bc3f6a
Your Name <you@example.com>
2021-12-21 09:56:04 +0600

    adding all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import Home from "./components/Home/Home/Home";
+import React, { createContext, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import './App.css';
+import AddAdmin from './components/Dashboard/AddAdmin/AddAdmin';
+import AddFeature from './components/Dashboard/AddFeature/AddFeature.js';
+import AddReview from './components/Dashboard/AddReview/AddReview';
+import AddService from './components/Dashboard/AddService/AddService';
+import ManageOrder from './components/Dashboard/ManageOrder/ManageOrder';
+import Order from './components/Dashboard/Order/Order'
+import OrderList from './components/Dashboard/OrderList/OrderList';
+ import Home from './components/Home/Home/Home';
+import Login from './components/Login/Login';
+// import NotMatched from './components/NotMatched/NotMatched';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import Footer from './components/Shared/Footer/Footer';
+import Navbar from './components/Shared/Navbar/Navbar'; 
 
-export default function App() {
+export const UserContext = createContext();
+function App() {
+  const [userData, setUserData] = useState({});
+  useEffect(() => {
+    const data = JSON.parse(sessionStorage.getItem('user'))
+    if (data) {
+      setUserData(data);
+    }
+    else {
+      setUserData({});
+    }
+  }, [])
+
+
   return (
-    <Router>
-        <Switch>
-          <Route path="/about">
-            
-          </Route>
-          <Route path="/users">
-            
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-    </Router>
-  );
+    <UserContext.Provider value={[userData, setUserData]} >
+      <Router>
+        <ScrollToTop>
+          <Navbar />
+          <Switch>
+            <PrivateRoute path="/dashboard/order-list">
+              <OrderList />
+            </PrivateRoute>
+            <PrivateRoute path="/dashboard/add-feature" >
+              <AddFeature />
+            </PrivateRoute>
+            <PrivateRoute path="/dashboard/add-review" >
+              <AddReview />
+            </PrivateRoute>
+            <PrivateRoute path="/dashboard/add-service" >
+              <AddService />
+            </PrivateRoute>
+            <PrivateRoute path="/dashboard/order/:id" >
+              <Order />
+            </PrivateRoute>
+            <PrivateRoute path="/dashboard/order" >
+              <Order />
+            </PrivateRoute>
+            <PrivateRoute path="/dashboard/add-admin" >
+              <AddAdmin />
+            </PrivateRoute>
+            <PrivateRoute path="/dashboard/manage-order" >
+              <ManageOrder />
+            </PrivateRoute>
+            <Route path="/login" >
+              <Login />
+            </Route>
+            <Route path="/home">
+              <Redirect to='/' />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+          <Footer />
+        </ScrollToTop>
+      </Router>
+    </UserContext.Provider>
+  )
 }
+
+export default App;
